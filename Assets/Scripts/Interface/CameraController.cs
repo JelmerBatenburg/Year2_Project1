1cce62a2fb3ad4a33d8fb838575100135f006a05
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2018-09-06 22:53:56 +0200

    FirstPush
    -added the unity scene and files
    -added a camera movement script
    -started working on the tower placement-grid

---
+++ Assets/Scripts/Interface/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform cam;
+    [HideInInspector]
+    public int current;
+    public int max;
+    public float cameraUpMovement;
+    public float cameraUpRotation;
+    public float camspeed;
+
+    public void Update()
+    {
+        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical")) * Time.deltaTime * camspeed);
+        if (Input.GetButton("Fire3"))
+        {
+            transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X"), 0));
+        }
+
+        if(Input.GetAxisRaw("Mouse ScrollWheel") != 0)
+        {
+            if(current == 0 && -Input.GetAxisRaw("Mouse ScrollWheel") < 0 || current == max && -Input.GetAxisRaw("Mouse ScrollWheel") > 0)
+            {
+
+            }
+            else
+            {
+                int add = (-Input.GetAxisRaw("Mouse ScrollWheel") > 0) ? 1 : -1;
+                current += add;
+                transform.position += new Vector3(0, cameraUpMovement, 0) * add;
+                cam.transform.Rotate(Vector3.right * cameraUpRotation * add);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tower/TowerGrid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerGrid : MonoBehaviour {
+
+    public GroundScanArea[] scannableArea;
+    public bool drawGizmos;
+    public Color gizmoColor;
+    public int towerSize;
+    public List<TowerTile> tiles;
+
+    public void Update()
+    {
+        
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        if(scannableArea.Length != 0 && drawGizmos)
+        {
+            for (int i = 0; i < scannableArea.Length; i++)
+            {
+                Gizmos.DrawWireCube(scannableArea[i].position, scannableArea[i].size);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class TowerTile
+{
+    public bool taken;
+    public Vector3 location;
+}
+
+[System.Serializable]
+public class GroundScanArea
+{
+    public Vector3 size;
+    public Vector3 position;
+}
